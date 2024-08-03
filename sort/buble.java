89fcd063f95f8efa253b50d6036a092f627ca7fe
Saurav Ashok Mahajan <110321501+kashifsorravmahajan@users.noreply.github.com>
2024-08-03 14:19:43 +0530

    Add files via upload

---
+++ sort/buble.java
@@ -0,0 +1,29 @@
+package sort;
+
+public class buble {
+    public static void printArray (int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+            
+        }
+        System.out.println();
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,2,8,9,7,6};
+        // bnubble sort
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j]>arr[j+1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+                
+            }
+        }
+        printArray(arr);
+    }
+    
+}
